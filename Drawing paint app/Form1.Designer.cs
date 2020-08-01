2ae3f937e658fa54245a90134319937c1d14d13d
Marc Raymond <59947352+mkr123456@users.noreply.github.com>
2020-08-01 16:42:46 -0700

    paintcolorapp

---
+++ Drawing paint app/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace paintproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(447, 371);
+            this.Name = "Form1";
+            this.Text = "My Picture";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+        private void paintproject.Form1.InitializeComponent()
+        {
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveImageAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Drawing paint app/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paintproject
+{
+    public partial class Form1 : Form
+
+    {
+        private readonly Bitmap bmp = new Bitmap(1024, 768);
+        private readonly Pen p = new Pen(Color.Black, 5);
+        private readonly bool drawing = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1.InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Drawing_paint_app)
+                Drawing_paint_app = false;
+            else
+            Drawing_paint_app = true;
+
+        }
+    }
+   
